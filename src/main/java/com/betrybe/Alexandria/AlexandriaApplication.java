c879d70bfc9f3b6b2002ada5ab62f8ca9d46924b
gabriel <soutogabriel04@gmail.com>
2024-02-07 14:22:08 -0300

    feat: Book repository/service

--- src/main/java/com/betrybe/Alexandria/AlexandriaApplication.java
+++ src/main/java/com/betrybe/Alexandria/AlexandriaApplication.java
@@ -2,12 +2,18 @@ package com.betrybe.Alexandria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.betrybe.Alexandria.models.entities")
+@EnableJpaRepositories("com.betrybe.alexandria.models.repositories")
+@ComponentScan("com.betrybe.alexandria")
 public class AlexandriaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlexandriaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AlexandriaApplication.class, args);
+  }
 
 }
---
+++ src/main/java/com/betrybe/Alexandria/models/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.betrybe.Alexandria.models.repositories;
+
+import com.betrybe.Alexandria.models.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
