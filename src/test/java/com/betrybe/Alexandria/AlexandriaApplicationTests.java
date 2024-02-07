4ac84ee76769d88456ef3806037115696b3c5d04
gabriel <soutogabriel04@gmail.com>
2024-02-07 12:39:23 -0300

    feat: initial commit

---
+++ src/test/java/com/betrybe/Alexandria/AlexandriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.Alexandria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
