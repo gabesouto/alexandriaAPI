d61ab5d64cb3b4433f824f3fdf613878d98c1352
gabriel <soutogabriel04@gmail.com>
2024-02-08 12:51:22 -0300

    feat: Author entitiy / Author repository

---
+++ src/main/java/com/betrybe/Alexandria/models/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.betrybe.Alexandria.models.repositories;
+
+import com.betrybe.Alexandria.models.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
