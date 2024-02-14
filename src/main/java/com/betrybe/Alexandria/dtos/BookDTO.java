98bf86af7f3cd3defcc472522978d16c5c6fa49c
gabriel <soutogabriel04@gmail.com>
2024-02-14 16:25:07 -0300

    feat: update dtos to match new relations

--- src/main/java/com/betrybe/Alexandria/dtos/BookDTO.java
+++ src/main/java/com/betrybe/Alexandria/dtos/BookDTO.java
@@ -1,12 +1,21 @@
 package com.betrybe.Alexandria.dtos;
 
+import com.betrybe.Alexandria.models.entities.Author;
 import com.betrybe.Alexandria.models.entities.Book;
 import com.betrybe.Alexandria.models.entities.BookDetail;
+import com.betrybe.Alexandria.models.entities.Publisher;
+import java.util.List;
 
-public record BookDTO(Long id, String title, String genre) {
-  
+public record BookDTO(
+    Long id,
+    String title,
+    String genre,
+    Publisher publisher,
+    BookDetail bookDetail,
+    List<Author> authorList) {
+
   public Book toBook() {
-    return new Book(id, title, genre);
+    return new Book(id, title, genre, null, null, null);
   }
 
 }
